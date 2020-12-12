23d87573494cfaf8764d4d7ae10355ba4864b39d
MERTULAS <67822910+MERTULAS@users.noreply.github.com>
2020-12-12 19:52:24 +0300

    new array methods added

--- JS-array-methods/script.js
+++ JS-array-methods/script.js
@@ -114,3 +114,55 @@ const transportation = data.reduce((obj, item) => {
 }, {});
 
 console.log(transportation);
+
+const people2 = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+  ];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+  ];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const some = people2.some(p => {
+        return (new Date()).getFullYear() - p.year >= 19;
+});
+console.log(some);
+
+// Array.prototype.every() // is everyone 19 or older?
+const every = people2.every(p => {
+    return (new Date()).getFullYear() - p.year >= 19;
+});
+console.log(every);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+const find = comments.find(comment => comment.id == 823423);
+console.log(find);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+const indexNumber = comments.findIndex(comment => comment.id == 823423);
+console.log(indexNumber);
+
+// delete the comment with the ID of 823423
+const deleted = comments.splice(indexNumber, 1);
+console.log(deleted);
+console.log(comments);
+
+// OR
+
+const newComments = [
+    ...comments.slice(0, indexNumber),
+    ...comments.slice(indexNumber + 1)
+]
+console.log(newComments);
